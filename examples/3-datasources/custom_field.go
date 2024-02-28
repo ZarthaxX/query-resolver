ba965d1faaef05a56967221c6ca44bac15d4efc3
ZarthaxX <tomastropeaa@gmail.com>
2024-02-27 21:28:39 -0300

    introduced new types

--- examples/3-datasources/custom_field.go
+++ examples/3-datasources/custom_field.go
@@ -12,47 +12,47 @@ func (o OrderID) Equal(other OrderID) bool {
 	return o == other
 }
 
-type ServiceStart = value.Primitive[int64]
+type ServiceStart = value.PrimitiveArithmetic[int64]
 
 var ServiceStartName engine.FieldName = "service.start"
 var ServiceStartField = operator.NewField(ServiceStartName)
 
 func NewServiceStart(v int64) ServiceStart {
-	return value.NewPrimitive[int64](v)
+	return value.NewPrimitiveArithmetic(v)
 }
 
-type ServiceAmount = value.Primitive[int64]
+type ServiceAmount = value.PrimitiveArithmetic[int64]
 
 var ServiceAmountName engine.FieldName = "service.amount"
 var ServiceAmountField = operator.NewField(ServiceAmountName)
 
 func NewServiceAmount(v int64) ServiceAmount {
-	return value.NewPrimitive[int64](v)
+	return value.NewPrimitiveArithmetic(v)
 }
 
-type OrderStatus = value.Primitive[string]
+type OrderStatus = value.PrimitiveComparable[string]
 
 var OrderStatusName engine.FieldName = "order.status"
 var OrderStatusField = operator.NewField(OrderStatusName)
 
 func NewOrderStatus(v string) OrderStatus {
-	return value.NewPrimitive[string](v)
+	return value.NewPrimitiveComparable(v)
 }
 
-type OrderType = value.Primitive[string]
+type OrderType = value.PrimitiveComparable[string]
 
 var OrderTypeName engine.FieldName = "order.type"
 var OrderTypeField = operator.NewField(OrderTypeName)
 
 func NewOrderType(v string) OrderType {
-	return value.NewPrimitive[string](v)
+	return value.NewPrimitiveComparable(v)
 }
 
-type DriverName = value.Primitive[string]
+type DriverName = value.PrimitiveComparable[string]
 
 var DriverNameName engine.FieldName = "driver.name"
 var DriverNameField = operator.NewField(DriverNameName)
 
 func NewDriverName(v string) DriverName {
-	return value.NewPrimitive[string](v)
+	return value.NewPrimitiveComparable(v)
 }
